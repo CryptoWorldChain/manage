25717d4715c1d85da6349fc6db21796ebb8327ce
sean <sean@finchain360.com>
2018-06-20 18:51:37 +0800

    1

--- src/main/java/org/brewchain/manage/impl/GetNodeInfoImpl.java
+++ src/main/java/org/brewchain/manage/impl/GetNodeInfoImpl.java
@@ -7,8 +7,8 @@ import java.util.TimeZone;
 
 import org.brewchain.account.core.AccountHelper;
 import org.brewchain.account.core.BlockChainConfig;
-import org.brewchain.account.gens.Act.Account;
-import org.brewchain.account.gens.Act.AccountTokenValue;
+import org.brewchain.evmapi.gens.Act.Account;
+import org.brewchain.evmapi.gens.Act.AccountTokenValue;
 import org.brewchain.bcapi.gens.Oentity.OValue;
 import org.brewchain.manage.dao.ManageDaos;
 import org.brewchain.manage.gens.Manageimpl.PMANModule;
